c9a9906e96bfbde4cabb1235bfcaaf25e1a87c65
Moses Solomon <mosessolomon@mosess-mbp.lan>
2024-01-25 12:48:14 -0500

    popup not working

---
+++ content.js
@@ -0,0 +1,10 @@
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    // hey
+    if (request.action === 'convert') {
+      document.documentElement.style.filter = 'grayscale(100%)';
+      sendResponse({ message: 'Website converted to black and white.' });
+    } else if (request.action === 'reset') {
+      document.documentElement.style.filter = 'none';
+      sendResponse({ message: 'Website reset to original colors.' });
+    }
+  });
